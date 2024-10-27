51bb605a4dc8e9c5089a6b94854d4b075fd1ed5d
Youssef HALILY <halily.youssef@gmail.com>
2024-10-27 08:35:31 +0100

    Update ColorSensor.java

--- src/perception/ColorSensor.java
+++ src/perception/ColorSensor.java
@@ -1,5 +1,30 @@
 package perception;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.Color;
 
-public class ColorSensor {
+public class ColorSensor extends EV3ColorSensor {
+    public ColorSensor(Port port) {
+        super(port);
+    }
 
+  
+    public String getCurrentColor() {
+        int colorId = getColorID();
+        switch (colorId) {
+            case Color.RED: return "Rouge";
+            case Color.GREEN: return "Vert";
+            case Color.BLUE: return "Bleu";
+            case Color.YELLOW: return "Jaune";
+            case Color.BLACK: return "Noir";
+            case Color.WHITE: return "Blanc";
+            case Color.GRAY: return "Gris";
+            default: return "Inconnu";
+        }
+    }
+
+   
+    public String changeColor() {
+        return getCurrentColor();
+    }
 }
