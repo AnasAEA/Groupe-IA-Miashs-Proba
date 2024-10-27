9e5795d30bd3df4d0d16baa396ce91030154675a
Youssef HALILY <halily.youssef@gmail.com>
2024-10-27 08:37:09 +0100

    Update Pince.java

--- src/action/Pince.java
+++ src/action/Pince.java
@@ -1,5 +1,31 @@
 package action;
 
-public class Pince {
 
+import lejos.hardware.motor.NXTRegulatedMotor;
+
+public class Pince {
+    private NXTRegulatedMotor motor;
+    private boolean etatFerme;
+
+    public Pince(NXTRegulatedMotor motor) {
+        this.motor = motor;
+        this.etatFerme = false; 
+    }
+
+ 
+    public void saisirPalet() {
+        motor.rotate(-360 * 3); 
+        etatFerme = true;
+    }
+
+
+    public void lacherPalet() {
+        motor.rotate(360 * 3);
+        etatFerme = false;
+    }
+
+
+    public void setEtat(boolean etat) {
+        etatFerme = etat;
+    }
 }
