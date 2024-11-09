b95aa6e89acb2e339c6594ff627a978320210498
Youssef HALILY <halily.youssef@gmail.com>
2024-11-09 19:35:32 +0100

    Update Pince.java

--- src/action/Pince.java
+++ src/action/Pince.java
@@ -1,31 +1,58 @@
 package action;
 
-
 import lejos.hardware.motor.NXTRegulatedMotor;
 
 public class Pince {
-    private NXTRegulatedMotor motor;
-    private boolean etatFerme;
+    
+    private NXTRegulatedMotor pinceMotor;
+    private boolean etat; // true si les pinces sont fermées, false si ouvertes
 
     public Pince(NXTRegulatedMotor motor) {
-        this.motor = motor;
-        this.etatFerme = false; 
+        this.pinceMotor = motor;
+        this.pinceMotor.setSpeed(motor.getSpeed() * 2); // Ajustement de la vitesse
+        this.etat = true; // Initialisé comme fermé
     }
 
- 
+    // Active les pinces pour saisir un palet
     public void saisirPalet() {
-        motor.rotate(-360 * 3); 
-        etatFerme = true;
+        if (!etat) { // Si les pinces ne sont pas déjà fermées
+            pinceMotor.rotate(-360 * 3); // Ferme les pinces (ajustez la rotation si nécessaire)
+            while (pinceMotor.isMoving()) {}
+            etat = true;
+            System.out.println("Palet saisi.");
+        }
     }
 
-
+    // Relâche le palet
     public void lacherPalet() {
-        motor.rotate(360 * 3);
-        etatFerme = false;
+        if (etat) { // Si les pinces ne sont pas déjà ouvertes
+            pinceMotor.rotate(360 * 3); // Ouvre les pinces
+            while (pinceMotor.isMoving()) {}
+            etat = false;
+            System.out.println("Palet relâché.");
+        }
     }
 
-
+    // Modifie l'état des pinces
     public void setEtat(boolean etat) {
-        etatFerme = etat;
+        if (etat) {
+            saisirPalet();
+        } else {
+            lacherPalet();
+        }
     }
-}
+
+    // Vérifie si les pinces sont fermées
+    public boolean isClose() {
+        return etat;
+    }
+
+    // Vérifie si les pinces sont ouvertes
+    public boolean isOpen() {
+        return !etat;
+    }
+
+    // Permet de définir manuellement l'état des pinces
+    public void setClosed(boolean etat) {
+        this.etat = etat;
+    }
