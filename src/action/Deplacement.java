9db2d006c675c4888d800a6b7a466ef7d1f7bdc5
ANAS <62776704+AnasAEA@users.noreply.github.com>
2024-11-11 10:06:40 +0100

    Update Deplacement.java

--- src/action/Deplacement.java
+++ src/action/Deplacement.java
@@ -2,17 +2,22 @@ package action;
 
 
 import lejos.hardware.motor.Motor;
-import lejos.utility.Delay;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.localization.OdometryPoseProvider;
+import lejos.robotics.localization.PoseProvider;
 import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.navigation.Pose;
 import lejos.robotics.chassis.*;
 
+
+
 public class Deplacement {
+	
 	private static final double DIAMETRE = 5.6;
   	private MovePilot pilot;
-	private float direction =0;
+    	private PoseProvider poseProvider;
 	
 	
 	public Deplacement() {
@@ -20,35 +25,40 @@ public class Deplacement {
 		Wheel roue2 = WheeledChassis.modelWheel((RegulatedMotor)Motor.A, DIAMETRE).offset(6.2);
 		Chassis chassis = new WheeledChassis(new Wheel[] {roue1, roue2}, WheeledChassis.TYPE_DIFFERENTIAL);
 		pilot = new MovePilot(chassis);
+        poseProvider = new OdometryPoseProvider(pilot); // Initialisation du PoseProvider
+
 	}
 	
-	public float getDirection() { //renvoie la direction en degres
-		return direction;
-	}
+    	public float getDirection() { // Renvoie la direction en degrés
+        	Pose pose = poseProvider.getPose();
+       		return pose.getHeading(); // Récupère l'orientation actuelle du robot
+    	}
 	
 	public MovePilot getPilot() { 
 		return pilot;
 	}
 
-	public void avancerAsync(double distance) { //Avancement asynchrone de la distance en paramètres
+	public void avancerSync(double distance) { //Ava ncement asynchrone de la distance en paramètres
    		 pilot.travel(distance, true); 
+	}
+	
+	public void avancer(double distance) { //Avancement asynchrone de la distance en paramètres
+  		 pilot.travel(distance); 
 	}
 
 	public void tournerAsync(double angle) { //Rotation asynchrone de l’angle en paramètre
 	        pilot.rotate(angle, true);
-	        direction += angle;
-	        direction = direction % 360;
+	        // Pas besoin de mettre à jour 'direction' ici ; le PoseProvider s'en charge
     	}
 
 	public void modifVitRot(double s){ //modifie la vitesse de rotation en degrés par seconde
 		pilot.setAngularSpeed(s);
 	}
-
-	 public void modifVitLin(double s){ //modifie la vitesse de déplacement linéaire en cm par seconde.
-                 pilot.setLinearSpeed(s);    
 	
+	public boolean isMoving() { 
+		 return pilot.isMoving();
 	 }
-	
+
 	public void stop() {
 		pilot.stop();
 	}
