b58c099d53f4f92d49790dca0c3f2de06dd9a70c
nguiranm <nguiranemarieme@gmail.com>
2024-11-15 20:44:50 +0100

    Update Agent.java

--- src/action/Agent.java
+++ src/action/Agent.java
@@ -119,6 +119,12 @@ public class Agent {
         lacherPalet();
         deplacement.avancer(-10);
     }
+	 public void versCouleur(String color) {
+    	while (!(couleur.getColorName() == color)) {
+    		deplacement.avancerAsync(directionCampAdverse);
+    	}
+    	deplacement.stop();	
+    }
 
     public boolean ChercherPalet() {
         ArrayList<float[]> objets = detecterLesObjets();
@@ -167,7 +173,52 @@ public class Agent {
 	   //on avance jusqu'à que la couleur detectée est Black (ligne centrale)
 	   versCouleur("Black");
     }
-
+	 
+    public void premierPalet() {
+    	while (!(capteurTouche.isPressed())) {
+    	deplacement.avancerAsync(directionCampAdverse);
+    	//esquive();
+    	}
+    	deplacement.stop();
+    	pince.ouvrirPince();
+    	pince.fermerPince();
+    	deplacement.tournerAsync(90);
+    	deplacement.avancerAsync(15);
+    	deplacement.tournerAsync(90);
+    	Agent a = new Agent();
+    	a.versCouleur("White");
+    	deplacement.avancerAsync(5);
+    	pince.ouvrirPince();
+    	System.out.print( "le palet est déposé");
+    	pince.fermerPince();
+    }
+     public void secondPalet() {
+    	 deplacement.tournerAsync(180);
+    	 Agent a = new Agent();
+     	 a.versCouleur("Bleu");
+     	deplacement.tournerAsync(90);
+     	while (!(capteurUltrason.detecterPalet())){
+     		deplacement.avancerAsync(100);
+     	}
+     	while (!(capteurTouche.isPressed())) {
+        	deplacement.avancerAsync(directionCampAdverse);
+        	//esquive();
+        	}
+        	deplacement.stop();
+        	pince.ouvrirPince();
+        	pince.fermerPince();
+        	deplacement.tournerAsync(90);
+        	Agent b = new Agent();
+        	b.versCouleur("White");
+        	deplacement.avancerAsync(5);
+        	pince.ouvrirPince();
+        	System.out.print( "le palet est déposé");
+        	pince.fermerPince();
+        }
+     public void troisiemePalet() {
+    	 Agent c = new Agent();
+    	 c.secondPalet();
+     }
     public void run() {
         // Initialiser la direction du camp adverse
         directionCampAdverse = deplacement.getDirection();
