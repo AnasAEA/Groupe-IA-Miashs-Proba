5722ffc0a270d0282c594bf2589fa1fcdeb98605
yousraBites <103833319+yousraBites@users.noreply.github.com>
2024-11-17 10:41:50 +0100

    Update Deplacement.java

--- src/action/Deplacement.java
+++ src/action/Deplacement.java
@@ -48,7 +48,7 @@ public class Deplacement {
 	}
 
 	public void tournerAsync(double angleToTurn) { //Rotation asynchrone de l’angle en paramètre
-		this.getPilot().setAngularSpeed(30);
+		this.getPilot().modifVitRot(30);
 		if (angleToTurn > 0) {
               	       angleToTurn -= 7; // If turning clockwise, reduce angle
             	} else if (angleToTurn < 0) {
