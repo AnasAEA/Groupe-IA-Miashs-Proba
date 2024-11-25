e9bd3033ef241660fc5ce50482f38a1a064ab4ff
ANAS <62776704+AnasAEA@users.noreply.github.com>
2024-11-25 14:02:34 +0100

    Update Pince.java

--- src/action/Pince.java
+++ src/action/Pince.java
@@ -9,20 +9,20 @@ public class Pince {
     
     private NXTRegulatedMotor pince;
     private int vitesseMoteur = 1000;
-    private boolean etat; // true si les pinces sont fermées, false si ouvertes
+    private boolean etat; // true si les pinces sont Ouvertes, false sinon
 
     public Pince() {
         pince = Motor.D;
         pince.setSpeed(vitesseMoteur);
-        this.etat = true; // Initialisé comme fermé
+        this.etat = false; // Initialisé comme fermé
     }
 
     // Active les pinces pour saisir un palet
     public void fermerPince() {
         if(etat) {
-	        pince.rotate(-1800);
+		pince.rotate(-850);
 	        while (pince.isMoving()) {
-	            Delay.msDelay(100); // Wait for 100 milliseconds
+	            Delay.msDelay(100); 
 	        }
 	        etat=false;
 	        }
@@ -32,7 +32,7 @@ public class Pince {
     // Relâche le palet
     public void ouvrirPince() {
         if(!etat) {
-        pince.rotateTo(1800);
+        pince.rotateTo(850);
         while (pince.isMoving()) {
             Delay.msDelay(100); // Wait for 100 milliseconds
         }
@@ -42,7 +42,6 @@ public class Pince {
     }
 
     public void setPincesOuvertes(boolean pincesOuvertes) { 
-
 		this.etat = pincesOuvertes;
 	}
 
